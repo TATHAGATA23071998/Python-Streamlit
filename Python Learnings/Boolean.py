4f8e375f77b01330da2f7489ec0a6321a1809da5
Tathagata Chakraborty <tathagata4059@gmail.com>
2026-04-11 02:19:45 +0530

    Add files via upload

---
+++ Python Learnings/Boolean.py
@@ -0,0 +1,14 @@
+# Boolean fns
+print(type(True))
+print(type(False))
+print(1 > 2)  # Output: False
+print(type(None))
+print(bool(""))
+print(bool(0))
+
+email = "tathagata4059@gmail.com"
+phone = "1234567890"
+username = ""
+print(all([email, phone, username]))  # Output: False
+print(any([email, phone, username]))  # Output: True
+
