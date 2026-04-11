111f5f86fd74e9371d5bb722fa64eb7d0f5dca89
Tathagata Chakraborty <tathagata4059@gmail.com>
2026-04-11 23:21:13 +0530

    Add files via upload

---
+++ Python Learnings/Comparison.py
@@ -0,0 +1,18 @@
+print (1==1)
+print (1==2)
+print (1!=2)
+print (1>2)
+print (1<2)
+print (1>=1)
+print (1<=1)
+print (1>2 and 2>3)
+print (1>2 or 2>3)
+print (not 1>2)
+print (1==1 and 2==2)
+print (1==1 or 2==3)
+
+
+print(1>4>5)
+
+age = input ("Enter your age: ")
+print(18<=int(age)<=60)
